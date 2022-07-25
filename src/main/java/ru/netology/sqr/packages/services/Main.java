10b14c0818b0378fd7b338c1833dd77414a352e0
Сергей Поздов <torres.t2012@yandex.ru>
2022-07-25 11:15:39 +0300

    Initial commit

--- src/main/java/ru/netology/sqr/packages/services/Main.java
+++ src/main/java/ru/netology/sqr/packages/services/Main.java
@@ -1,8 +1,7 @@
 package ru.netology.sqr.packages.services;
-
 public class Main {
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        System.out.println(service.calculateSQR(200, 300));
+        S
     }
 }
